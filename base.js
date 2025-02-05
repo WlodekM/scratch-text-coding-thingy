303e27cae15821ff112717f34766886152f036fb
WlodekM <wlodzio.maly@gmail.com>
2025-02-05 21:12:17 +0200

    base(d).js

---
+++ base.js
@@ -0,0 +1,9 @@
+// base blocks, for including in ur projects
+await import('./tw-blocks/blocks_vertical/control.js');
+await import('./tw-blocks/blocks_vertical/event.js');
+await import('./tw-blocks/blocks_vertical/looks.js');
+await import('./tw-blocks/blocks_vertical/motion.js');
+await import('./tw-blocks/blocks_vertical/operators.js');
+await import('./tw-blocks/blocks_vertical/sound.js');
+await import('./tw-blocks/blocks_vertical/sensing.js');
+// no data.js since that gets auto-imported as it is needed for var declaration
